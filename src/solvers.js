b74aedf13439add8a7dc75bb68582a0bab85f71c
franklinjjeng <franklinjjeng@gmail.com>
2016-12-21 21:18:12 -0800

    Pass rooks counter solution

--- src/solvers.js
+++ src/solvers.js
@@ -16,51 +16,44 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = [];
   // find a starting point
   // check if the next position is available to place a piece
   // if it is available put a piece in that location
     //check next position
   //when we have the expected number of pieces 
-  console.log('n: ' + n);
   var tempBoard = [];
-  var row = [];
 
   // initializing board
-  for (var i = 0; i < n; i++) {
-    tempBoard.push([]);
-    console.log('tempBoard', tempBoard);
-    
-    for (var j = 0; j < n; j++) {
-      console.log('i', i, 'j', j);
-      tempBoard[i].push(0);
-    }
-  }
+  // for (var i = 0; i < n; i++) {
+  //   tempBoard.push([]);
 
-  var board = new Board(tempBoard);
+  //   for (var j = 0; j < n; j++) {
+  //     tempBoard[i].push(0);
+  //   }
+  // }
+
+  var board = new Board({'n': n});
 
   for (var i = 0; i < n; i++) {
     for (var j = 0; j < n; j++) {
       board.attributes[i][j] = 1;
-      console.log('board', board.attributes[i]);
-      console.log('row', board.hasAnyRowConflicts());
-      console.log('col', board.hasAnyColConflicts());
       if (board.hasAnyRowConflicts() || board.hasAnyColConflicts()) {
-        console.log('conflict detected');
         board.attributes[i][j] = 0;
       }
     }
   }
-
   return board.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  var countRooks = function (n) {
+    if (n === 0) {
+      return 1;
+    }
+    return n *= countRooks(n - 1);
+  };
+  return countRooks(n);
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
